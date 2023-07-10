cd1691310876a6f53aca045319c2719a4592740f
jhy979 <jhy979@woowahan.com>
2023-07-10 21:29:33 +0900

    feat: 블로그 초기 설정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
-  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
-  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  title: 'jhy979.com',
+  description: '개발일기',
+  language: 'ko', // 'ko', 'en' => currently support versions for Korean and English
+  siteUrl: 'https://www.jhy979.com',
+  ogImage: '', // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: '', // 'jhy979/jhy979-gatsby-blog',
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: '장호영',
     bio: {
-      role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      role: '개발자',
+      description: ['함께 웃어야 행복한', '코딩을 최대한 피하는', '비즈니스 가치를 최우선시하는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: 'https://github.com/jhy979',
+      linkedIn: 'https://www.linkedin.com/in/%ED%98%B8%EC%98%81-%EC%9E%A5-344002255/',
+      email: 'jhy979@gmail.com',
     },
   },
 
@@ -42,15 +42,15 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
+      // {
+      //   date: '2021.02 ~',
+      //   activity: '개인 블로그 개발 및 운영',
+      //   links: {
+      //     post: '/gatsby-starter-jhy979-introduction',
+      //     github: 'https://github.com/jhy979/jhy979-gatsby-blog',
+      //     demo: 'https://www.jhy979.com',
+      //   },
+      // },
     ],
 
     projects: [
@@ -71,18 +71,18 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
+      // {
+      //   title: '개발 블로그 테마 개발',
+      //   description:
+      //     '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
+      //   techStack: ['gatsby', 'react'],
+      //   thumbnailUrl: 'blog.png',
+      //   links: {
+      //     post: '/gatsby-starter-jhy979-introduction',
+      //     github: 'https://github.com/jhy979/jhy979-gatsby-blog',
+      //     demo: 'https://www.jhy979.com',
+      //   },
+      // },
     ],
   },
 };
--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -4,15 +4,7 @@ import './style.scss';
 function PageFooter({ author, githubUrl }) {
   return (
     <footer className="page-footer-wrapper">
-      <p className="page-footer">
-        © {new Date().getFullYear()}
-        &nbsp;
-        <a href={githubUrl}>{author}</a>
-        &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-          &nbsp;zoomkoding-gatsby-blog
-        </a>
-      </p>
+      <p className="page-footer">© {new Date().getFullYear()} 나인 블로그</p>
     </footer>
   );
 }
